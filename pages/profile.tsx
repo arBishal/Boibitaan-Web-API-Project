ba976df2d74959ac9ac2709df4131cf2184b9a65
magic-kiri <kimuapon@gmail.com>
2022-05-19 21:42:21 +0600

    rename path

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import ProfileBody from "./components/ProfileBody";
-import profileStyle from "./components/profileBody.module.css";
 import Footer from "./components/Footer";
 
 const Profile = () => {

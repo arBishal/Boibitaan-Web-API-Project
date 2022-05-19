98e521b59b6d4cafa8e9da58ddb02b88c9d5b41f
Bishal <arbishal.319@gmail.com>
2022-05-19 18:09:58 +0600

    tweaks in home

--- pages/components/HomeBody.tsx
+++ pages/components/HomeBody.tsx
@@ -4,7 +4,14 @@ import bodyStyle from "./homeBody.module.css";
 function HomeBody() {
   return (
     <div className={bodyStyle.homeBody}>
-      <h4 style={{ fontSize: "24px" }}>হোম!</h4>
+      <div className={bodyStyle.sectionHeader}>
+        <h4 style={{ fontSize: "20px", margin: "0px" }}>
+          সর্বাধিক বিক্রিত বইসমূহ
+        </h4>
+      </div>
+      <div className={bodyStyle.sectionHeader}>
+        <h4 style={{ fontSize: "20px", margin: "0px" }}>আমাদের সকল বইসমূহ</h4>
+      </div>
     </div>
   );
 }

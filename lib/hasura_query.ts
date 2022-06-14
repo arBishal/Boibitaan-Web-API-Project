65d39e3620f61aa0c4219b69ddeb8ac21473f144
magic-kiri <kimuapon@gmail.com>
2022-06-15 05:47:53 +0600

    complete sign in

--- lib/hasura_query.ts
+++ lib/hasura_query.ts
@@ -11,8 +11,29 @@ export const findUserByEmail = (
       insert_user(
         objects: { email: "${email}", name: "${name}", passHash: "${passHash}", phone: "${phone}" }
         on_conflict: { constraint: user_email_key }
+        ) {
+          affected_rows
+        }
+      }
+      `;
+};
+
+export const checkAccountWithEmailPassHash = (
+  email: string,
+  passHash: string
+) => {
+  return gql`
+    query MyQuery {
+      user(
+        where: {
+          email: { _eq: "${email}" }
+          passHash: { _eq: "${passHash}" }
+        }
       ) {
-        affected_rows
+        id
+        name
+        phone
+        email
       }
     }
   `;
--- pages/logIn.tsx
+++ pages/logIn.tsx
@@ -6,9 +6,36 @@ import Image from "next/image";
 import landinglogo from "../public/logo/logolanding.png";
 import Button from "./src/ui-custom-components/Button";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { sha256 } from "js-sha256";
 
 function LogIn() {
   const router = useRouter();
+
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const handleSubmit = async () => {
+    if (email === undefined || email === "") {
+      alert("Please provide an email!");
+    } else if (password === undefined || password === "") {
+      alert("Please provide a password!");
+    } else {
+      const res = await axios.post("/api/login", {
+        email,
+        passHash: sha256(password),
+      });
+      if (res.status === 200) {
+        const { verdict, token, message } = res.data;
+        if (verdict) {
+          localStorage.setItem("token", token);
+        } else {
+          alert(message);
+        }
+      }
+    }
+  };
+
   return (
     <div className={signUpStyle.signUpPage}>
       <div className={signUpStyle.signUpForm}>
@@ -22,15 +49,26 @@ function LogIn() {
         </div>
         <div className={signUpStyle.signUpInput}>
           <p style={{ marginBottom: "3px" }}> ই-মেইল </p>
-          <InputText placeholder="ই-মেইল লিখুন"></InputText>
+          <InputText
+            id="email"
+            placeholder="ই-মেইল লিখুন"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setEmail(e.target.value);
+            }}
+          ></InputText>
         </div>
         <div className={signUpStyle.signUpInput}>
           <p style={{ marginBottom: "3px" }}> পাসওয়ার্ড </p>
-          <InputPassword placeholder="পাসওয়ার্ড দিন"></InputPassword>
+          <InputPassword
+            id="password"
+            placeholder="পাসওয়ার্ড দিন"
+            onChange={(e) => setPassword(e.target.value)}
+          ></InputPassword>
         </div>
         <Button
           style={{ marginTop: "20px", marginBottom: "-15px" }}
-          onClick={() => router.push("/home")}
+          onClick={handleSubmit}
         >
           লগ ইন
         </Button>

c9ee2c96c17e395a68d71c9d037ff4fc55002d6c
magic-kiri <kimuapon@gmail.com>
2022-06-15 02:14:33 +0600

    complete server-side sign up

--- pages/api/signUp.ts
+++ pages/api/signUp.ts
@@ -1,15 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { gql } from "@apollo/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import client from "../../lib/apollo-client";
+import { findUserByEmail } from "../../lib/hasura_query";
 
 type Data = {
-  name: string
-}
+  verdict: boolean;
+  message: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const {name, email, passHash, phone} = req.body;
-  console.log(name, passHash);
-  res.status(200).json({ name: 'John Doe' })
+  const { email, name, passHash, phone } = req.body;
+  try {
+    const dbResponse = await client.mutate({
+      mutation: findUserByEmail(email, name, passHash, phone),
+    });
+    if (dbResponse?.data.insert_user.affected_rows === 0) {
+      res.status(200).json({ verdict: false, message: "Use different Email!" });
+    } else {
+      res.status(200).json({ verdict: false, message: "Sign Up successfull" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }

831b3995516328f7ea7adee53cf8c3c152b7f17d
magic-kiri <kimuapon@gmail.com>
2022-08-20 03:10:42 +0600

    add types from DB

--- lib/types.ts
+++ lib/types.ts
@@ -3,9 +3,43 @@ export type CredentialType = {
   passHash: string;
 };
 
-export type UserInformationType = {
+export type User = {
   id: string;
   name: string;
   email: string;
   phone: string;
+  accountNumber: number;
 };
+
+export type BankAccount = {
+  id: number;
+  name: string;
+  accountNumber: number;
+  balance: number;
+};
+
+export type Book = {
+  id: number;
+  name: string;
+  author: string;
+  price: number;
+  image: string;
+  publisher: string;
+  supplier: string;
+  quantity: string;
+  sale: string;
+  catagory: string;
+};
+
+export type Supplier = {
+  id: number;
+  name: string;
+  accountNumber: number;
+};
+
+export type Transaction = {
+  id: number;
+  sender: string;
+  reciever: string;
+  amount: number;
+}

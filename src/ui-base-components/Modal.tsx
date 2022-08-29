d9cacf0f1dcf08cce0119113b364dad47a4212f8
magic-kiri <kimuapon@gmail.com>
2022-08-30 00:46:46 +0600

    add type

--- src/ui-base-components/Modal.tsx
+++ src/ui-base-components/Modal.tsx
@@ -4,20 +4,19 @@ import { Modal as AntdModal } from "antd";
 
 type ModalProps = React.ComponentProps<typeof AntdModal> & {};
 
-export const error = (title, content) => {
+export const error = (title: string, content: string) => {
   AntdModal.error({
     title: title,
     content: content,
-    okText: 'ঠিক আছে'
+    okText: "ঠিক আছে",
   });
 };
 
-
-export const success = (title, content) => {
+export const success = (title: string, content: string) => {
   AntdModal.success({
     title: title,
     content: content,
-    okText: 'ঠিক আছে',
+    okText: "ঠিক আছে",
   });
 };
 
